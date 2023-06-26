abc2fdf3aef43e074bb13fab64c717b9d8a5dfc5
EmanueleVerita <emanuele.verita@gmail.com>
2023-06-26 16:14:12 +0200

    milestone 0

---
+++ js/script.js
@@ -0,0 +1,18 @@
+// creo gli array coi nomi ruoli e stringhe immagini
+
+const names = ['Wayne Barnett' , 'Angela Caroll' , 'Walter Gordon' , 'Angela Lopez' , 'Scott Estrada' , 'Barbara Ramos'];
+
+const roles = ['Founder & CEO' , 'Chief Editor' , 'Office Manager' , 'Social Media Manager' , 'Developer' , 'Graphic Designer'];
+
+const images = [' wayne-barnett-founder-ceo.jpg' , 'angela-caroll-chief-editor.jpg' , 'walter-gordon-office-manager.jpg' , 'angela-lopez-social-media-manager.jpg' , 'scott-estrada-developer.jpg' , 'barbara-ramos-graphic-designer.jpg'];
+
+//creo l'ogetto con dentro le strighe
+
+const teamInfo = {
+    names: [],
+    roles: [],
+    images: [],
+}
+
+
+
