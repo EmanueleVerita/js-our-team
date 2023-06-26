411a8c8ce92e712fc864de88c143ac858f94b9e0
EmanueleVerita <emanuele.verita@gmail.com>
2023-06-26 17:43:07 +0200

    milestone 3 provvisoria

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@
 const teamInfo = [
     {
         name: 'Wayne Barnett',
-        role: 'Found er&CEO',
+        role: 'Founder&CEO',
         immagine: 'wayne-barnett-founder-ceo.jpg'
     },
 
@@ -39,11 +39,17 @@ const teamInfo = [
     }
     
 ]
-
+/*
 teamInfo[0].name
 teamInfo[0]['name']
 
+*/
+
 for (const key in teamInfo) {
     console.log(key , teamInfo[key]);
+
+    
 }
 
+const printTeamInfo = document.querySelector('div');
+printTeamInfo.innerHTML = JSON.stringify(teamInfo[0]) + ' ' + JSON.stringify(teamInfo[1] + ' ' + JSON.stringify(teamInfo[2]) + ' ' + JSON.stringify(teamInfo[3]) + ' ' + JSON.stringify(teamInfo[4]) + ' ' + JSON.stringify(teamInfo[5]));
